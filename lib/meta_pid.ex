71df10b9a1663bd8d8d06636ae4624fe22dbcc5d
Chris Wilhelm <cwilhelm@avvo.com>
2016-08-01 14:40:47 -0700

    define metapid as a macro to specify struct and server name

--- lib/meta_pid.ex
+++ lib/meta_pid.ex
@@ -1,69 +1,74 @@
 defmodule MetaPid do
-  use GenServer
+  defmacro __using__(into: into, name: name) do
+    quote bind_quoted: [ into: into, name: name ] do
+      use GenServer
 
-  @server_name :meta_pid
+      @server_name name
+      @into into
 
-  def start_link(options \\ []) do
-    GenServer.start_link(__MODULE__, nil, name: @server_name)
-  end
-
-  def stop() do
-    GenServer.stop(@server_name)
-  end
-
-  def register_pid(pid, data \\ %{}) do
-    GenServer.call(@server_name, {:register_pid, pid, data})
-  end
-
-  def put_pid(pid, data) do
-    GenServer.call(@server_name, {:update_pid, pid, data})
-  end
-
-  def fetch_pid(pid) do
-    GenServer.call(@server_name, {:fetch_pid, pid})
-  end
-
-  def unregister_pid(pid) do
-    GenServer.call(@server_name, {:unregister_pid, pid})
-  end
-
-  def get_registry() do
-    GenServer.call(@server_name, :get_registry)
-  end
-
-  def init(_) do
-    {:ok, %{}}
-  end
-
-  def handle_call({:register_pid, pid, data}, _from, registry) do
-    exit_callback(pid)
-    {:reply, :ok, Map.put(registry, pid, data)}
-  end
-
-  def handle_call({:update_pid, pid, data}, _from, registry) do
-    # handle not present case?
-    {:reply, :ok, Map.put(registry, pid, data)}
-  end
-
-  def handle_call({:unregister_pid, pid}, _from, registry) do
-    {:reply, :ok, Map.delete(registry, pid)}
-  end
-
-  def handle_call({:fetch_pid, pid}, _from, registry) do
-    {:reply, Map.fetch(registry, pid), registry}
-  end
-
-  def handle_call(:get_registry, _from, registry) do
-    {:reply, registry, registry}
-  end
-
-  defp exit_callback(pid) do
-    spawn(fn ->
-      Process.monitor(pid)
-
-      receive do
-        {:DOWN, _, _, _, _} -> __MODULE__.unregister_pid(pid)
+      def start_link(options \\ []) do
+        GenServer.start_link(__MODULE__, nil, name: @server_name)
       end
-    end)
+
+      def stop() do
+        GenServer.stop(@server_name)
+      end
+
+      def register_pid(pid, data \\ struct(@into)) do
+        GenServer.call(@server_name, {:register_pid, pid, data})
+      end
+
+      def put_pid(pid, data) do
+        GenServer.call(@server_name, {:update_pid, pid, data})
+      end
+
+      def fetch_pid(pid) do
+        GenServer.call(@server_name, {:fetch_pid, pid})
+      end
+
+      def unregister_pid(pid) do
+        GenServer.call(@server_name, {:unregister_pid, pid})
+      end
+
+      def get_registry() do
+        GenServer.call(@server_name, :get_registry)
+      end
+
+      def init(_) do
+        {:ok, %{}}
+      end
+
+      def handle_call({:register_pid, pid, data}, _from, registry) do
+        exit_callback(pid)
+        {:reply, :ok, Map.put(registry, pid, data)}
+      end
+
+      def handle_call({:update_pid, pid, data}, _from, registry) do
+        # handle not present case?
+        {:reply, :ok, Map.put(registry, pid, data)}
+      end
+
+      def handle_call({:unregister_pid, pid}, _from, registry) do
+        {:reply, :ok, Map.delete(registry, pid)}
+      end
+
+      def handle_call({:fetch_pid, pid}, _from, registry) do
+        {:reply, Map.fetch(registry, pid), registry}
+      end
+
+      def handle_call(:get_registry, _from, registry) do
+        {:reply, registry, registry}
+      end
+
+      defp exit_callback(pid) do
+        spawn(fn ->
+          Process.monitor(pid)
+
+          receive do
+            {:DOWN, _, _, _, _} -> __MODULE__.unregister_pid(pid)
+          end
+        end)
+      end
+    end
   end
 end
--- test/meta_pid_test.exs
+++ test/meta_pid_test.exs
@@ -2,8 +2,16 @@ defmodule MetaPidTest do
   use ExUnit.Case
   doctest MetaPid
 
+  defmodule MyTestStruct do
+    defstruct [:foo]
+  end
+
+  defmodule MetaPidMap do
+    use MetaPid, into: MyTestStruct, name: :meta_pid
+  end
+
   setup do
-    {:ok, pid} = MetaPid.start_link()
+    {:ok, pid} = MetaPidMap.start_link()
 
     %{server: pid}
   end
@@ -18,71 +26,71 @@ defmodule MetaPidTest do
 
   test "stopping kills the process", %{server: pid} do
     assert Process.alive?(pid)
-    MetaPid.stop()
+    MetaPidMap.stop()
     refute Process.alive?(pid)
   end
 
   test "initializes an empty map for the registry" do
-    assert MetaPid.get_registry() == Map.new
+    assert MetaPidMap.get_registry() == Map.new
   end
 
   test "adds a new pid to the registry" do
     pid = new_link()
 
-    MetaPid.register_pid(pid)
+    MetaPidMap.register_pid(pid)
 
-    assert Map.has_key?(MetaPid.get_registry(), pid)
+    assert Map.has_key?(MetaPidMap.get_registry(), pid)
   end
 
   test "a pid's data can be retrieved once registered" do
     pid = new_link()
 
-    assert :error == MetaPid.fetch_pid(pid)
+    assert :error == MetaPidMap.fetch_pid(pid)
 
-    MetaPid.register_pid(pid)
+    MetaPidMap.register_pid(pid)
 
-    assert {:ok, _} = MetaPid.fetch_pid(pid)
+    assert {:ok, _} = MetaPidMap.fetch_pid(pid)
   end
 
   test "a pid can be optionally registered with data" do
     pid = new_link()
 
-    my_data = %{foo: :bar}
+    my_data = %MyTestStruct{foo: :bar}
 
-    MetaPid.register_pid(pid, my_data)
+    MetaPidMap.register_pid(pid, my_data)
 
-    assert MetaPid.fetch_pid(pid) == {:ok, my_data}
+    assert MetaPidMap.fetch_pid(pid) == {:ok, my_data}
   end
 
   test "removes a pid from the registry" do
     pids = Enum.map(0..10, fn (_) -> new_link() end)
 
     pids
-    |> Enum.each(fn(pid) -> MetaPid.register_pid(pid) end)
+    |> Enum.each(fn(pid) -> MetaPidMap.register_pid(pid) end)
 
     [to_remove | remaining] = pids
 
-    MetaPid.unregister_pid(to_remove)
+    MetaPidMap.unregister_pid(to_remove)
 
-    assert (MetaPid.get_registry() |> Map.keys) == remaining
+    assert (MetaPidMap.get_registry() |> Map.keys) == remaining
   end
 
   test "allows updates of data for a pid" do
     pids     = Enum.map(0..10, fn (_) -> new_link() end)
-    data     = %{foo: :bar}
-    new_data = %{asdf: :fdsa}
+    data     = %MyTestStruct{foo: :bar}
+    new_data = %MyTestStruct{foo: :new_value}
 
-    pids |> Enum.each(fn (pid) -> MetaPid.register_pid(pid, data) end)
+    pids |> Enum.each(fn (pid) -> MetaPidMap.register_pid(pid, data) end)
 
     [to_change | remaining] = pids
 
-    MetaPid.put_pid(to_change, new_data)
+    MetaPidMap.put_pid(to_change, new_data)
 
     remaining |> Enum.each(fn (pid) ->
-      assert MetaPid.fetch_pid(pid) == {:ok, data}
+      assert MetaPidMap.fetch_pid(pid) == {:ok, data}
     end)
 
-    assert MetaPid.fetch_pid(to_change) == {:ok, new_data}
+    assert MetaPidMap.fetch_pid(to_change) == {:ok, new_data}
   end
 
   test "when a process dies, its key is removed from the registry" do
@@ -90,11 +98,11 @@ defmodule MetaPidTest do
       spawn(fn -> :timer.sleep(1) end)
     end)
 
-    pids |> Enum.each(&MetaPid.register_pid/1)
+    pids |> Enum.each(&MetaPidMap.register_pid/1)
 
     :timer.sleep(10)
 
-    assert MetaPid.get_registry() == %{}
+    assert MetaPidMap.get_registry() == %{}
   end
 
   test "pid is automatically unregistered if its process terminates before callback is set" do
@@ -106,10 +114,10 @@ defmodule MetaPidTest do
 
     Process.exit(pid, :kill)
 
-    MetaPid.register_pid(pid)
+    MetaPidMap.register_pid(pid)
 
     :timer.sleep(1)
 
-    assert :error == MetaPid.fetch_pid(pid)
+    assert :error == MetaPidMap.fetch_pid(pid)
   end
 end
