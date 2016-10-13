7b68088a43c9f5fb509c3574a9fff9ef4c0a643f
Chris Wilhelm <cwilhelm@avvo.com>
2016-10-12 17:28:21 -0700

    Mix file prep for hex publication

--- mix.exs
+++ mix.exs
@@ -6,10 +6,30 @@ defmodule MetaPid.Mixfile do
       app: :meta_pid,
       version: "0.1.0",
       elixir: "~> 1.3",
+      description: description(),
+      package: package(),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
       dialyzer: [plt_add_deps: :transitive, plt_file: ".local.plt"]
+    ]
+  end
+
+  def description do
+    """
+    Library providing scaffolding for storing process-specific information
+    for duration of process' lifespan
+    """
+  end
+
+  defp package do
+    [
+      name: :meta_pid,
+      maintainers: ["Avvo, Inc", "Chris Wilhelm"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/avvo/meta_pid"
+      }
     ]
   end
 
